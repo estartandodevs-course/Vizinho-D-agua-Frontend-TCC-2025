1ff71799fe9b4d0e1cff485049e9cd6661dfdd3d
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-11-23 15:31:17 -0300

    refactor(pages, components): prepara página para API e adiciona componente de carregamento

--- src/pages/Comunidade/Comunidade.tsx
+++ src/pages/Comunidade/Comunidade.tsx
@@ -2,23 +2,43 @@
 import Abas from "../../components/abas/abas";
 import BarraTopo from "../../components/BarraTopo/BarraTopo";
 import Busca from "../../components/Busca/Busca";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mockComunidades, type Comunidade } from "../../mocks/comunidades.mock";
 import CardComunidade from "../../components/CardComunidade/CardComunidade";
+import Carregando from "../../components/Carregando/Carregando";
 
 export default function Comunidade() {
     const abasDaPagina = ["Explorar", "Seguindo"];
     const[abaAtiva, setAbaAtiva] = useState(abasDaPagina[0]);
     const [busca, setBusca] = useState("");
 
-    function filtrarComunidade(lista: Comunidade[], aba: string) {
-        const filtroBusca = (comunidade: Comunidade) => comunidade.title.toLowerCase().includes(busca.toLowerCase());
+    const [comunidade, setComunidade] = useState<Comunidade[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [erro, setError] = useState("");
 
-        if(aba === "Explorar") return lista.filter(c => !c.isSeguindo && filtroBusca(c));
-        if(aba === "Seguindo") return lista.filter(c => c.isSeguindo && filtroBusca(c));
+    useEffect(() => {
+        async function carregarComunidade() {
+            try{
+                await new Promise((resolve) => setTimeout(resolve, 300));
+            }catch(error) {
+                console.error("Erro ao carregar comunidades:", error);
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        carregarComunidade();
+    }, []);
+
+    function filtrarComunidade(lista: Comunidade[], aba: string) {
+        const filtroBusca = (c: Comunidade) =>
+            c.title.toLowerCase().includes(busca.toLowerCase());
+        if (aba === "Explorar") return lista.filter(c => !c.isSeguindo && filtroBusca(c));
+        if (aba === "Seguindo") return lista.filter(c => c.isSeguindo && filtroBusca(c));
         return lista.filter(filtroBusca);
     }
-    const comunidadesFiltradas = filtrarComunidade(mockComunidades, abaAtiva);
+    const comunidadeFiltrada = filtrarComunidade(mockComunidades, abaAtiva);
+
 
     function handleTrocarAba(novaAba: string) {
         setAbaAtiva(novaAba);
@@ -31,13 +51,20 @@ export default function Comunidade() {
             title="Comunidades"
             iconType="menu" />
             <Busca placeholder="Buscar por uma comunidade" onSearch={(valor) => setBusca(valor)} />
+
             <nav>
             <Abas listaDeAbas={abasDaPagina}
             abaAtiva={abaAtiva}
             onAbaClick={handleTrocarAba} />
             </nav>
+
+            {loading && <Carregando />}
+
+            {erro && <p>{erro}</p>}
+
+            {!loading && !erro && (
             <section className="lista-comunidades-container">
-                {comunidadesFiltradas.map(comunidade =>
+                {comunidadeFiltrada.map(comunidade =>
                     <CardComunidade
                         key={comunidade.id}
                         id={comunidade.id}
@@ -47,12 +74,13 @@ export default function Comunidade() {
                         members={comunidade.members}
                     />
                 )}
-                {comunidadesFiltradas.length === 0 &&
+                {comunidadeFiltrada.length === 0 &&
                     <p>
                         Nenhuma comunidade encontrada.
                     </p>
                 } 
             </section>
+            )}
         </>
     );
 }
